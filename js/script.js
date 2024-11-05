d459447c7a924a14ab25ab9fb34e7d538d12f476
Matt <154559729+davis-matt0@users.noreply.github.com>
2024-11-05 10:11:50 -0500

    add javascript functionality

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let homeScoreElement = document.getElementById("home-score");
+let guestScoreElement = document.getElementById("guest-score");
+
+let homeScore = 0;
+let guestScore = 0;
+
+let homePlusOne = document.getElementById("home-plus-one");
+let homePlusTwo = document.getElementById("home-plus-two");
+let homePlusThree = document.getElementById("home-plus-three");
+
+let guestPlusOne = document.getElementById("guest-plus-one");
+let guestPlusTwo = document.getElementById("guest-plus-two");
+let guestPlusThree = document.getElementById("guest-plus-three");
+
+homePlusOne.addEventListener("click", () => {
+  homeScore += 1;
+  homeScoreElement.textContent = homeScore;
+});
+
+homePlusTwo.addEventListener("click", () => {
+  homeScore += 2;
+  homeScoreElement.textContent = homeScore;
+});
+
+homePlusThree.addEventListener("click", () => {
+  homeScore += 3;
+  homeScoreElement.textContent = homeScore;
+});
+
+guestPlusOne.addEventListener("click", () => {
+  guestScore += 1;
+  guestScoreElement.textContent = guestScore;
+});
+
+guestPlusTwo.addEventListener("click", () => {
+  guestScore += 2;
+  guestScoreElement.textContent = guestScore;
+});
+
+guestPlusThree.addEventListener("click", () => {
+  guestScore += 3;
+  guestScoreElement.textContent = guestScore;
+});
